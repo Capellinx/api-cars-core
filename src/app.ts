fc2821f217573f72e5e1b0dda2763936abd49a5d
capellinx <capellaaa7@gmail.com>
2025-02-05 23:40:30 -0300

    feat: create instance route swagger UI

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
-import express, { json }  from "express"
+import 'express-async-errors'
+import express, { json } from "express"
+import helmet from 'helmet'
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+import { swaggerOptions } from './config/swagger'
 
-export const app = express()
+const app = express()
+const swaggerDocs = swaggerJSDoc(swaggerOptions)
 
 app.use(json())
+app.use(helmet())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
+export { app }
