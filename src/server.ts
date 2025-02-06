7cabf0c0aa3797200d019df2e72a9d79c20d84ec
capellinx <capellaaa7@gmail.com>
2025-02-05 23:16:45 -0300

    feat: create class to start database and server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,27 @@
+import { prisma } from "../prisma/prisma";
 import { app } from "./app";
+import { env } from "./env";
+   class Server {
+   constructor(){}
 
+   private async startDatabase(){
+      try {
+         await prisma.$connect();
+         console.log("🎲 Connected to database");
+      } catch (error) {
+         throw new Error(`❌ Failed to connect to database -> ${error}`);
+      }
+   }
 
-app.listen(process.env.PORT, () => {
-   console.log(`🚀 API sucessfully started at port ${process.env.PORT}`);
-})
+   public async startServer(){
+      const connectToDatabase = this.startDatabase.bind(this);
+
+      await connectToDatabase();
+
+      app.listen(env.PORT, () => {
+         console.log(`🚀 Server started on port ${env.PORT}`)
+      });
+   }
+}
+
+new Server().startServer();
