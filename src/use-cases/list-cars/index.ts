1f85bc0c24e93ae7fbbae7b8e9585d0764b62230
capellinx <capellaaa7@gmail.com>
2025-02-07 01:05:38 -0300

    feat: create use case to list all cars

---
+++ src/use-cases/list-cars/index.ts
@@ -0,0 +1,21 @@
+import { BhutService } from "../../infra/service/bhut.service";
+import { AuthenticateUserUseCase } from "../authenticate-user/authenticate-user";
+import { ListCarsUseCase } from "./list-cars";
+import { ListCarsController } from "./list-cars.controller";
+
+const bhutService = new BhutService()
+
+const authenticateUserUseCase = new AuthenticateUserUseCase(
+   bhutService
+)
+
+const listCarsUseCse = new ListCarsUseCase(
+   bhutService,
+   authenticateUserUseCase
+)
+
+const listCarsController = new ListCarsController(
+   listCarsUseCse
+)
+
+export { listCarsController }
