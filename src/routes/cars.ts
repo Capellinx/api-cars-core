4f0c6dc98e8d58301a3491b0d488af5a14d18d5c
capellinx <capellaaa7@gmail.com>
2025-02-07 10:01:16 -0300

    feat: create route to register new car

--- src/routes/cars.ts
+++ src/routes/cars.ts
@@ -2,14 +2,25 @@ import { Request, Response, Router } from "express";
 import { listCarsController } from "../use-cases/list-cars";
 import { DtoRequestValidationMiddleware } from "../middlewares/data-transfer-object-validation.middleware";
 import { listCarsSchema } from "../use-cases/list-cars/list-cars-dto";
+import { createCarController } from "../use-cases/create-car";
+import { createCarSchema } from "../use-cases/create-car/create-car-dto";
 
 export const carsRouter = Router()
 
 carsRouter.get(
    "/api/car",
-   DtoRequestValidationMiddleware.execute({ body: listCarsSchema }),
+   DtoRequestValidationMiddleware.execute({ query: listCarsSchema }),
    async (request: Request, response: Response) => {
       await listCarsController.handle(request, response)
+      return
+   }
+)
+
+carsRouter.post(
+   "/api/car",
+   DtoRequestValidationMiddleware.execute({ body: createCarSchema }),
+   async (request: Request, response: Response) => {
+      await createCarController.handle(request, response)
       return
    }
 )
