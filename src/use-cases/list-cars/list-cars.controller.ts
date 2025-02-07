07810a23c127233f0fdbe69014e88aa22913e2ba
capellinx <capellaaa7@gmail.com>
2025-02-07 15:00:47 -0300

    feat: update variables to english

--- src/use-cases/list-cars/list-cars.controller.ts
+++ src/use-cases/list-cars/list-cars.controller.ts
@@ -4,17 +4,17 @@ import { ListCarsUseCase } from "./list-cars-use-case";
 export class ListCarsController {
    constructor(
       private listCarsUseCase: ListCarsUseCase
-   ){}
+   ) { }
 
    async handle(request: Request, response: Response): Promise<Response> {
-      const { pagina: page, tamanhoPagina: pageSize, ativo: active } = request.query;
+      const {  page, pageSize, active } = request.query;
 
       const cars = await this.listCarsUseCase.execute({
          page: Number(page),
          pageSize: Number(pageSize),
          active: active === "true" ? true : false
-      })
+      });
 
-      return response.status(200).json(cars)
+      return response.status(200).json(cars);
    }
 }
