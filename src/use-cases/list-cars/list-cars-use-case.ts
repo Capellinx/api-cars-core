8a22c7af7cfc27763eb2ef311b1a7f46f91948c8
capellinx <capellaaa7@gmail.com>
2025-02-07 10:03:18 -0300

    feat: create new name to use case

---
+++ src/use-cases/list-cars/list-cars-use-case.ts
@@ -0,0 +1,44 @@
+import { env } from "../../config/env";
+import { CarsService } from "../../domain/service/cars.service";
+import { BadRequestError } from "../../utils/errors";
+import { AuthenticateUserUseCase } from "../authenticate-user/authenticate-user";
+import { ListCarsDTO } from "./list-cars-dto";
+
+export class ListCarsUseCase {
+   constructor(
+      private carsSeriCarsService: CarsService,
+      private authenticateUseCase: AuthenticateUserUseCase
+   ) { }
+
+   async execute({ page, pageSize, active }: ListCarsDTO) {
+      if (page < 1 || pageSize < 1) throw new BadRequestError("Page and pageSize must be greater than zero.")
+
+      if (typeof active !== "boolean") throw new BadRequestError("Active must be a boolean.")
+
+      const { accessToken } = await this.authenticateUseCase.execute({
+         login: env.USERNAME,
+         password: env.PASSWORD
+      })
+
+      const { items, paginacao } = await this.carsSeriCarsService.findAll({
+         page,
+         pageSize,
+         active,
+         accessToken
+      })
+
+      return {
+         items: items?.map((cars: any) => ({
+            id: cars.id,
+            nome: cars.nome,
+            marca: cars.marca,
+            preco: cars.preco,
+            anoFabricacao: cars.anoFabricacao,
+            ativo: cars.ativo,
+            criadoEm: cars.criadoEm,
+            atualizadnoEm: cars.atualizadnoEm
+         })),
+         paginacao,
+      }
+   }
+}
