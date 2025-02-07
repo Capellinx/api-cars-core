8086c1b23483087a836487d25356e7893aa9f081
capellinx <capellaaa7@gmail.com>
2025-02-07 00:21:53 -0300

    refactor: change name repository to service

---
+++ src/domain/service/cars.service.ts
@@ -0,0 +1,44 @@
+
+export interface CarsService {
+   findAll(props: CarsService.InputAllCars): Promise<CarsService.OutputAllCars>;
+   auth(props: CarsService.InputAuth): Promise<CarsService.OutputAuth>;
+}
+
+export namespace CarsService {
+   export interface InputAllCars {
+      page: number,
+      pageSize: number,
+      active: boolean
+      accessToken: string
+   }
+
+   export interface InputAuth {
+      login: string,
+      password: string
+   }
+
+   export interface OutputAuth {
+      accessToken: string,
+      refreshToken: string,
+      tokenType: string,
+      expiresIn: number,
+   }
+
+   export interface OutputAllCars { 
+      paginacao: {
+         pagina: number,
+         tamanhoPagina: number,
+         total: number,
+      },
+      items: {
+         id: string,
+         nome: string,
+         marca: string,
+         preco: number,
+         anoFabricacao: number
+         ativo: boolean
+         criadoEm: Date
+         atualizadnoEm: Date | null
+      }[]
+   }
+}
