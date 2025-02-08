1ed6353b7c4bc1b5b9890a61338ddfa2dbed278c
capellinx <capellaaa7@gmail.com>
2025-02-08 01:36:32 -0300

    feat: create service to publish information on queue

---
+++ src/use-cases/publish-create-inforamtion/publish-create-information-use-case.ts
@@ -0,0 +1,25 @@
+import { MessagingService } from "../../domain/service/messaging.service";
+
+export class PublishCreateInformationUseCase {
+   constructor(
+      private messagingService: MessagingService
+   ) { }
+
+   async execute({ car_id }: { car_id: string }) {
+      if (!car_id) throw new Error("car_id is required")
+
+      await this.messagingService.start()
+      
+      await this.messagingService.createQueue({ queueName: "car-events" })
+
+      await this.messagingService.publishInQueue({
+         queueName: "car-events",
+         message: JSON.stringify({
+            car_id,
+            created_at: new Date().toISOString()
+         }) 
+      })
+
+      return
+   }
+}
