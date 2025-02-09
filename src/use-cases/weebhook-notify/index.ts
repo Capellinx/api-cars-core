60d92f0911d2ac188658afa489515139f1f04df8
capellinx <capellaaa7@gmail.com>
2025-02-09 10:53:34 -0300

    feat: inject dependency on use case

--- src/use-cases/weebhook-notify/index.ts
+++ src/use-cases/weebhook-notify/index.ts
@@ -1,7 +1,12 @@
+import { MongooseRepository } from "../../infra/repositories/mongoose.repository";
 import { WebhookNotifyUseCase } from "./webhook-notify-use-case";
 import { WeebhookNotifyController } from "./webhook-notify.controller";
 
-const weebHookNotifyUseCase = new WebhookNotifyUseCase();
+const mongooseRepository = new MongooseRepository()
+
+const weebHookNotifyUseCase = new WebhookNotifyUseCase(
+   mongooseRepository
+)
 
 const weebhookNotifyController = new WeebhookNotifyController(
    weebHookNotifyUseCase
