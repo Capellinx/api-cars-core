6c7983f615288efc06795e711e1cfd74bff2c808
capellinx <capellaaa7@gmail.com>
2025-02-09 16:35:21 -0300

    refactor: update URL to connect webhook

--- src/infra/service/implementantion/api-car-core.service.ts
+++ src/infra/service/implementantion/api-car-core.service.ts
@@ -5,7 +5,7 @@ import { HistoryDTO } from "../../../use-cases/weebhook-notify/webhook-notify-dt
 export class ApiCarCoreService implements LogService {
    async registerLog({ car_id, created_at, process_at }: HistoryDTO): Promise<void> {
        try {
-         return await axios.post("http://localhost:3333/api/webhook", {
+          return await axios.post("http://localhost:3333/api/webhook", {
             car_id,
             created_at,
             process_at
