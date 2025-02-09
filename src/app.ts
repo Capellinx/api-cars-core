3ca3ce9a8246a2b9e2961577cfd587271bcddc21
capellinx <capellaaa7@gmail.com>
2025-02-09 10:42:01 -0300

    feat: register router webhook

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,9 @@ import helmet from 'helmet'
 import swaggerJSDoc from 'swagger-jsdoc'
 import swaggerUi from 'swagger-ui-express'
 import { swaggerOptions } from './config/swagger'
-import { HandleErrorsMiddleware } from './middlewares/handle-errors.middlewares'
 import { carsRouter } from './routes/cars'
 import { healthRouter } from './routes/health'
+import { weebhookRouter } from './routes/weebhook'
 
 const app = express()
 const swaggerDocs = swaggerJSDoc(swaggerOptions)
@@ -14,7 +14,7 @@ const swaggerDocs = swaggerJSDoc(swaggerOptions)
 app.use(json())
 app.use(helmet())
 
-app.use(carsRouter, healthRouter)
+app.use(carsRouter, healthRouter, weebhookRouter)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 export { app }
