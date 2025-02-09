45f49a7bf93ddc2e772325d7229e1cadd6baf78d
capellinx <capellaaa7@gmail.com>
2025-02-09 13:35:40 -0300

    feat: add new information on schema save webhook

--- src/infra/repositories/mongoose.repository.ts
+++ src/infra/repositories/mongoose.repository.ts
@@ -1,22 +1,28 @@
 import { model, Schema } from "mongoose";
 import { CarsRepository } from "../../domain/repositories/cars.repository";
-import { z } from "zod";
-import { historyLogSchema } from "../../use-cases/weebhook-notify/webhook-notify-schema";
+import { date, z } from "zod";
+import { historyLogSchema } from "../../use-cases/weebhook-notify/webhook-notify-dto";
 
 
 export class MongooseRepository implements CarsRepository {
-   async createLog(message: string): Promise<void> {
+   async createLog({car_id, created_at, process_at}: CarsRepository.Input): Promise<void> {
       const schema = new Schema<z.infer<typeof historyLogSchema>>({
-         message: String,
-         createdAt: {
+         car_id: String,
+         created_at: {
             type: Date,
-            default: Date.now
          },
+         process_at: {
+            type: Date,
+         }
       })
 
       const HistoryLog = model('HistoryLog', schema, 'history_logs')
 
-      const newHistoryLog = new HistoryLog({ message });
+      const newHistoryLog = new HistoryLog({ 
+         car_id,
+         created_at,
+         process_at
+       });
 
       await newHistoryLog.save()
 
