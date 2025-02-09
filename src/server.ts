ef8ed815290ebadff6e9d3c2859492df69728e7b
capellinx <capellaaa7@gmail.com>
2025-02-09 16:33:25 -0300

    feat: instance listenning queue

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { MongoConnection } from "../mongo/mongoose";
 import { app } from "./app";
 import { env } from "./config/env";
+import { consumeQueueUseCase } from "./use-cases/consume-queue";
 class Server {
    private mongoServer = new MongoConnection();
 
@@ -17,8 +18,14 @@ class Server {
 
       await connectToDatabase();
 
+
       app.listen(env.PORT, () => {
-         console.log(`🚀 Server started on port ${env.PORT}`)
+         try {
+            console.log(`🚀 Server started on port ${env.PORT}`)
+            consumeQueueUseCase.execute();
+         } catch (error) {
+            console.error(error);
+         }
       });
    }
 }
