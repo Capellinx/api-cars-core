35b5ca7d7e872c3a10c094f49efbc92ad6e5e1b5
capellinx <capellaaa7@gmail.com>
2025-02-09 16:35:43 -0300

    fix: resolve problem to reacreate schema inside method

--- src/infra/repositories/mongoose.repository.ts
+++ src/infra/repositories/mongoose.repository.ts
@@ -3,19 +3,20 @@ import { CarsRepository } from "../../domain/repositories/cars.repository";
 import { date, z } from "zod";
 import { historyLogSchema } from "../../use-cases/weebhook-notify/webhook-notify-dto";
 
+const schema = new Schema<z.infer<typeof historyLogSchema>>({
+   car_id: String,
+   created_at: {
+      type: Date,
+   },
+   process_at: {
+      type: Date,
+   }
+})
 
 export class MongooseRepository implements CarsRepository {
-   async createLog({car_id, created_at, process_at}: CarsRepository.Input): Promise<void> {
-      const schema = new Schema<z.infer<typeof historyLogSchema>>({
-         car_id: String,
-         created_at: {
-            type: Date,
-         },
-         process_at: {
-            type: Date,
-         }
-      })
+   constructor() { }
 
+   async createLog({car_id, created_at, process_at}: CarsRepository.Input): Promise<void> {
       const HistoryLog = model('HistoryLog', schema, 'history_logs')
 
       const newHistoryLog = new HistoryLog({ 
